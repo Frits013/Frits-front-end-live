2abca495e7aabfdd9a32d64b26d8283eb88a406d
Frits013 <kars.smeulders@fidds.nl>
2025-01-27 09:23:24 +0100

    Update client.ts
    updated anonkey

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://eqjsrvbisiuysboukgnt.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxanNydmJpc2l1eXNib3VrZ250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyMDc4ODAsImV4cCI6MjAyMTc4Mzg4MH0.Wd_7qYbJnXBXZBgdvGVrPiZZGBEcXhM_YHxwVWBtNGY';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxanNydmJpc2l1eXNib3VrZ250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjM5NDUsImV4cCI6MjA1MzI5OTk0NX0.IGfJjbGgH_gUnxGYFAlXg9Nj09nfgSxCoH0m6twrlm8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
